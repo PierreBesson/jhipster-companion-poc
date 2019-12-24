22fcfa395f0282059e6fd309b18fc0dfd70add4d
Pierre Besson <pierreb@liquidshare.io>
2019-12-24 18:05:12 +0100

    working poc with eureka

--- src/main/java/com/mycompany/myapp/web/rest/GatewayRouter.java
+++ src/main/java/com/mycompany/myapp/web/rest/GatewayRouter.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -25,29 +26,27 @@ public class GatewayRouter {
         this.discoveryLocatorProperties = discoveryLocatorProperties;
     }
 
-    @Bean
-    public RouteLocator routes(RouteLocatorBuilder builder) {
-        RouteLocatorBuilder.Builder routeLocatorBuilder = builder.routes();
-
-        List<String> services = discoveryClient.getServices();
-        for (String service : services) {
-            List<ServiceInstance> instances = discoveryClient.getInstances(service);
-            for (ServiceInstance instance : instances) {
-                String instance_route = String.format("%s/%s", service, instance.getInstanceId()).toLowerCase();
-                routeLocatorBuilder.route(instance_route, r -> {
-                    return r.path("/gateway/" + instance_route + "/**")
-                        .filters(f -> f.rewritePath("/gateway/" + instance_route + "/(?<remaining>.*)", "/${remaining}")).uri(instance.getUri());
-                });
-            }
-        }
-
-        return routeLocatorBuilder.build();
-    }
-//
+// Naive implementation that doesn't refresh
 //    @Bean
-//    public RouteDefinitionLocator jhipsterCompanionRouteDefinitionLocator(
-//        DiscoveryClient discoveryClient, DiscoveryLocatorProperties discoveryLocatorProperties) {
+//    public RouteLocator routes(RouteLocatorBuilder builder) {
+//        RouteLocatorBuilder.Builder routeLocatorBuilder = builder.routes();
 //
-//        return new JHipsterCompanionRouteDefinitionLocator(discoveryClient, discoveryLocatorProperties);
+//        discoveryClient.getServices().forEach(service -> {
+//            List<ServiceInstance> instances = discoveryClient.getInstances(service);
+//            instances.forEach(instance -> {
+//                String instance_route = String.format("%s/%s", service, instance.getInstanceId()).toLowerCase();
+//                routeLocatorBuilder.route(instance_route, r -> r.path("/gateway/" + instance_route + "/**")
+//                    .filters(f -> f.rewritePath("/gateway/" + instance_route + "/(?<remaining>.*)", "/${remaining}")).uri(instance.getUri()));
+//            });
+//        });
+//
+//        return routeLocatorBuilder.build();
 //    }
+//
+    @Bean
+    public RouteDefinitionLocator jhipsterCompanionRouteDefinitionLocator(
+        DiscoveryClient discoveryClient, DiscoveryLocatorProperties discoveryLocatorProperties) {
+
+        return new JHipsterCompanionRouteDefinitionLocator(discoveryClient, discoveryLocatorProperties);
+    }
 }
